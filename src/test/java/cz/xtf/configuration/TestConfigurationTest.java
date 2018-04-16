10ea9c0c4034a164fc286c4b057f727ee8dba64a
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-04-16 15:11:54 +0300

    Add test for TestConfiguration.class use system-rule for unit test;

--- src/test/java/cz/xtf/configuration/TestConfigurationTest.java
+++ src/test/java/cz/xtf/configuration/TestConfigurationTest.java
@@ -2,20 +2,42 @@ package cz.xtf.configuration;
 
 import cz.xtf.TestConfiguration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 public class TestConfigurationTest {
 
-	private static final String IMAGE_JDV_CLIENT = "IMAGE_JDV_CLIENT";
-
 	@Rule
 	public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
+	@Before
+	public void setEnvironmentVariables(){
+		environmentVariables.set("IMAGE_JDV_CLIENT", "IMAGE_JDV_CLIENT");
+		environmentVariables.set("MASTER_USERNAME", "MASTER_USERNAME");
+	}
+
 	@Test
-	public void canReadEnvironmentVariableFromTestConfiguration(){
-		environmentVariables.set(IMAGE_JDV_CLIENT,IMAGE_JDV_CLIENT);
-		Assert.assertEquals("Fix TestConfiguration constructor",IMAGE_JDV_CLIENT, TestConfiguration.imageJdvClient());
+	public void canReadEnvironmentVariableFromTestConfiguration() {
+
+		Assert.assertEquals("Fix TestConfiguration constructor", "IMAGE_JDV_CLIENT", TestConfiguration.imageJdvClient());
+	}
+
+	@Test
+	public void canReadEnvironmentVariableByTestConfigurationFromXTFConfiguration() {
+
+		Assert.assertEquals( "MASTER_USERNAME", TestConfiguration.masterUsername());
+	}
+
+
+	@Test
+	public void canReadDefaultValueOfPropertyFromTestConfiguration() {
+		Assert.assertEquals("6.2.1", TestConfiguration.getFuseVersion());
+	}
+
+	@Test
+	public void canReadDefaultValueByTestConfigurationFromXTFConfiguration() {
+		Assert.assertEquals(8080, TestConfiguration.vertxProxyPort());
 	}
 }
