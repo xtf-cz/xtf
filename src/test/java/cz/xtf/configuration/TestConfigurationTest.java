a25211705da123e2ed54639ff903135925549795
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-04-19 15:31:13 +0300

    delete test duplicate;

--- src/test/java/cz/xtf/configuration/TestConfigurationTest.java
+++ src/test/java/cz/xtf/configuration/TestConfigurationTest.java
@@ -10,9 +10,4 @@ public class TestConfigurationTest {
 	public void canReadDefaultValueOfPropertyFromXTFConfiguration() {
 		Assert.assertEquals("/usr/bin/oc", TestConfiguration.ocBinaryLocation());
 	}
-
-	@Test
-	public void canReadDefaultValueByTestConfigurationFromXTFConfiguration() {
-		Assert.assertEquals(8080, TestConfiguration.vertxProxyPort());
-	}
 }
