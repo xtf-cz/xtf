1df705892d446877cf671a5e20cb484ca281337b
Marek Schmidt <maschmid@redhat.com>
2018-04-27 16:31:35 +0200

    add getMasterToken static method

--- src/main/java/cz/xtf/openshift/OpenShiftUtils.java
+++ src/main/java/cz/xtf/openshift/OpenShiftUtils.java
@@ -1,9 +1,18 @@
 package cz.xtf.openshift;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+
 import cz.xtf.TestConfiguration;
+import cz.xtf.http.HttpClient;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 public class OpenShiftUtils {
 	private static OpenShiftUtil adminUtil;
 	private static OpenShiftUtil masterUtil;
@@ -70,6 +79,38 @@ public class OpenShiftUtils {
 			return new OpenShiftUtil(masterUrl, namespace, token);
 		} catch (MalformedURLException e) {
 			throw new IllegalStateException("OpenShift Master URL is malformed!");
+		}
+	}
+
+	public static String getMasterToken() {
+		if(TestConfiguration.openshiftOnline() || TestConfiguration.getMasterToken() != null) {
+			return TestConfiguration.getMasterToken();
+		}
+		else {
+			String masterUrl = TestConfiguration.masterUrl();
+			String username = TestConfiguration.masterUsername();
+			String password = TestConfiguration.masterPassword();
+
+			try {
+				String url = masterUrl + "/oauth/authorize?response_type=token&client_id=openshift-challenging-client";
+				List<Header> headers = HttpClient.get(url)
+						.basicAuth(username, password)
+						.preemptiveAuth()
+						.disableRedirect()
+						.responseHeaders();
+				Optional<Header> location = headers.stream().filter(h -> "Location".equals(h.getName())).findFirst();
+				if (location.isPresent()) {
+					log.debug("Location: {}", location.get().getValue());
+					String token = StringUtils.substringBetween(location.get().getValue(), "#access_token=", "&");
+					log.debug("Oauth token: {}", token);
+					return token;
+				}
+				log.debug("Location header with token not found");
+				return null;
+			} catch (IOException e) {
+				log.error("Error getting token from master", e);
+				return null;
+			}
 		}
 	}
 }
