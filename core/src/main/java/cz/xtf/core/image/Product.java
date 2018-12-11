7bf20fc17023f1a578ecfb000177149e3182c5e0
PavelDrobek <pdrobek@localhost.localdomain>
2018-12-11 19:36:22 +0100

    [product] Product class

---
+++ core/src/main/java/cz/xtf/core/image/Product.java
@@ -0,0 +1,38 @@
+package cz.xtf.core.image;
+
+import cz.xtf.core.config.XTFConfig;
+
+public class Product {
+	private final String id;
+
+	public Product(String id) {
+		this.id = id;
+	}
+
+	public Image image() {
+		return Image.resolve(id.replaceAll("\\..*", ""));
+	}
+
+	public String property(String propertyId) {
+		return resolveDefaultingProperty("properties." + propertyId);
+	}
+
+	public String version() {
+		return resolveDefaultingProperty("version");
+	}
+
+	public String templatesRepo() {
+		return resolveDefaultingProperty("templates.repo");
+	}
+
+	public String templatesBranch() {
+		return resolveDefaultingProperty("templates.branch");
+	}
+
+	private String resolveDefaultingProperty(String propertyId) {
+		String value = XTFConfig.get("xtf." + id + "." + propertyId);
+		String defaultingValue = id.contains(".") ? XTFConfig.get("xtf." + id.replaceAll("\\..*", "") + "." + propertyId) : null;
+
+		return value != null ? value : defaultingValue;
+	}
+}
---
+++ core/src/main/java/cz/xtf/core/image/Products.java
@@ -0,0 +1,11 @@
+package cz.xtf.core.image;
+
+import cz.xtf.core.config.XTFConfig;
+
+public class Products {
+
+	public static Product resolve(String id) {
+		String subId = XTFConfig.get("xtf." + id + ".subid");
+		return new Product(subId == null ? id : id + "." + subId);
+	}
+}
