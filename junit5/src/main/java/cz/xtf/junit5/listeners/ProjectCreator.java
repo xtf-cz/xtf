85ff8029db415ecab0b27dda8f1abe49728c945f
Marek Schmidt <maschmid@redhat.com>
2019-01-08 22:41:01 +0100

    [junit5] wait after project deletion

--- junit5/src/main/java/cz/xtf/junit5/listeners/ProjectCreator.java
+++ junit5/src/main/java/cz/xtf/junit5/listeners/ProjectCreator.java
@@ -3,9 +3,14 @@ package cz.xtf.junit5.listeners;
 import cz.xtf.core.config.OpenShiftConfig;
 import cz.xtf.core.openshift.OpenShift;
 import cz.xtf.core.openshift.OpenShifts;
+import cz.xtf.core.waiting.SimpleWaiter;
 import cz.xtf.junit5.config.JUnitConfig;
+
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.BooleanSupplier;
 
 public class ProjectCreator implements TestExecutionListener {
 	private static final OpenShift openShift = OpenShifts.master();
@@ -20,7 +25,12 @@ public class ProjectCreator implements TestExecutionListener {
 	@Override
 	public void testPlanExecutionFinished(TestPlan testPlan) {
 		if(JUnitConfig.cleanOpenShift()) {
-			openShift.deleteProject();
+			boolean deleted = openShift.deleteProject();
+			// For multi-module maven projects, other modules may attempt to crate project requests immediately after this modules deleteProject
+			if (deleted) {
+				BooleanSupplier bs = () -> openShift.getProject(OpenShiftConfig.namespace()) == null;
+				new SimpleWaiter(bs, TimeUnit.MINUTES, 2, "Waiting for old project deletion").waitFor();
+			}
 		}
 	}
 }
