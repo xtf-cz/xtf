c8ee6372fde23efe19c65a34dff55fbdce6ec689
Martin Choma <mchoma@10.0.0.24>
2020-01-07 11:38:08 +0100

    [#328] Make clean project timeout configurable

--- core/src/main/java/cz/xtf/core/config/WaitingConfig.java
+++ core/src/main/java/cz/xtf/core/config/WaitingConfig.java
@@ -5,9 +5,11 @@ import org.slf4j.event.Level;
 public class WaitingConfig {
 	public static final String WAITING_TIMEOUT = "xtf.waiting.timeout";
 	public static final String WAITING_LOG_LEVEL = "xtf.waiting.log.level";
+	public static final String WAITING_TIMEOUT_CLEANUP = "xtf.waiting.timeout.cleanup";
 
 	private static final String WAITING_TIMEOUT_DEFAULT = "180000";
 	private static final String WAITING_LOG_LEVEL_DEFAULT = "INFO";
+	private static final String WAITING_TIMEOUT_CLEANUP_DEFAULT = "20000";
 
 	public static long timeout() {
 		return Long.parseLong(XTFConfig.get(WAITING_TIMEOUT, WAITING_TIMEOUT_DEFAULT));
@@ -15,5 +17,9 @@ public class WaitingConfig {
 
 	public static Level level() {
 		return Level.valueOf(XTFConfig.get(WAITING_LOG_LEVEL, WAITING_LOG_LEVEL_DEFAULT).toUpperCase());
+	}
+
+	public static long timeoutCleanup() {
+		return Long.parseLong(XTFConfig.get(WAITING_TIMEOUT_CLEANUP, WAITING_TIMEOUT_CLEANUP_DEFAULT));
 	}
 }
