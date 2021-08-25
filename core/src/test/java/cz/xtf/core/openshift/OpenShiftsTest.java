bad4316a00fe0af977097939b05b10ad333f1238
Marco Carletti <mcarlett@redhat.com>
2021-08-25 15:09:50 +0200

    Add feature cache oc binary in local folder.

---
+++ core/src/test/java/cz/xtf/core/openshift/OpenShiftsTest.java
@@ -0,0 +1,53 @@
+package cz.xtf.core.openshift;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import cz.xtf.core.config.OpenShiftConfig;
+
+public class OpenShiftsTest {
+
+    static final String ocUrl = "https://ocp48console/amd64/linux/oc.tar";
+    static final String version = "4.8.2";
+    static File ocTarFile = null;
+    static Path expected = null;
+
+    @BeforeAll
+    public static void init() throws URISyntaxException {
+        ocTarFile = Paths.get(Thread.currentThread().getContextClassLoader().getResource("oc.tar.gz").toURI()).toFile();
+        expected = Paths.get(OpenShiftConfig.binaryCachePath(), version,
+                Base64.getEncoder().encodeToString(ocUrl.getBytes(StandardCharsets.UTF_8)).replace("=", ""));
+    }
+
+    @AfterEach
+    public void clean() throws IOException {
+        final File directory = Paths.get(OpenShiftConfig.binaryCachePath()).toFile();
+        if (directory.exists()) {
+            FileUtils.deleteDirectory(directory);
+        }
+    }
+
+    @Test
+    public void saveOnCacheTest() throws IOException {
+        OpenShifts.saveOcOnCache(version, ocUrl, ocTarFile);
+        Assertions.assertTrue(Paths.get(expected.normalize().toString(), ocTarFile.getName()).toFile().exists());
+    }
+
+    @Test
+    public void getOcFromCacheTest() throws IOException {
+        FileUtils.copyFile(ocTarFile, new File(expected.toFile(), ocTarFile.getName()));
+        Assertions.assertTrue(OpenShifts.getOcFromCache(version, ocUrl, ocTarFile).exists());
+    }
+
+}
