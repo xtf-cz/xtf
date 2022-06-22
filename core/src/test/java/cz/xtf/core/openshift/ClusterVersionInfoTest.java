b89663aeabc5c7193fb7fe0e5d5c57a6c2fc92b6
Marco Carletti <mcarlett@redhat.com>
2022-06-22 13:52:04 +0200

    [#495] Use stable channel if version not found

--- core/src/test/java/cz/xtf/core/openshift/ClusterVersionInfoTest.java
+++ core/src/test/java/cz/xtf/core/openshift/ClusterVersionInfoTest.java
@@ -1,11 +1,30 @@
 package cz.xtf.core.openshift;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import cz.xtf.core.config.OpenShiftConfig;
+import cz.xtf.core.config.XTFConfig;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
+import uk.org.webcompere.systemstubs.properties.SystemProperties;
+
+@ExtendWith(SystemStubsExtension.class)
 public class ClusterVersionInfoTest {
 
-    private ClusterVersionInfo clusterVersionInfo = new ClusterVersionInfo();
+    private ClusterVersionInfo clusterVersionInfo;
+
+    @SystemStub
+    private SystemProperties systemProperties;
+
+    @BeforeEach
+    public void setOcpVersion() {
+        systemProperties.set(OpenShiftConfig.OPENSHIFT_VERSION, "4.8.14");
+        XTFConfig.loadConfig();
+        clusterVersionInfo = ClusterVersionInfoFactory.INSTANCE.getClusterVersionInfo(true);
+    }
 
     @Test
     public void getOpenshiftVersionTest() {
